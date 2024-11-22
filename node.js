18309a61489f7920fc95a12fa5a543e91e761b43
KLIM-why <lebedinecevenij@gmail.com>
2024-11-22 20:27:48 +0200

    baehth

---
+++ node.js
@@ -0,0 +1,12 @@
+function filterProducts(category) {
+    const products = document.querySelectorAll('.product');
+    products.forEach(product => {
+        if (category === 'all') {
+            product.style.display = 'block'; 
+        } else if (product.classList.contains(category)) {
+            product.style.display = 'block'; 
+        } else {
+            product.style.display = 'none'; 
+        }
+    });
+}
